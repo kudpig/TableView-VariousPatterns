e5c4dea06cc4fe66c3b66055e0d380c06ba85804
kudpig <shin.kudo.aa@gmail.com>
2021-02-16 15:16:24 +0900

    [setting]storyboardの分割まで

---
+++ TableView-VariousPatterns/FirstTableView/FirstTableView.swift
@@ -0,0 +1,29 @@
+//
+//  FirstTableView.swift
+//  TableView-VariousPatterns
+//
+//  Created by shinichiro kudo on 2021/02/16.
+//
+
+import UIKit
+
+class FirstTableView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TableView-VariousPatterns/ViewController.swift
+++ TableView-VariousPatterns/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func tapGoFirst(_ sender: UIButton) {
+        performSegue(withIdentifier: "goFirst", sender: nil)
+    }
+    
 }
 
