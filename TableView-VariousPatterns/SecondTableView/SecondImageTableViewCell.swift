a73e774b311a0096afbae5b52373e4e8d187933c
kudpig <shin.kudo.aa@gmail.com>
2021-02-20 00:21:02 +0900

    [add]SecondImageTableViewCell(class&xib)

---
+++ TableView-VariousPatterns/SecondTableView/SecondImageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SecondImageTableViewCell.swift
+//  TableView-VariousPatterns
+//
+//  Created by shinichiro kudo on 2021/02/20.
+//
+
+import UIKit
+
+class SecondImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
